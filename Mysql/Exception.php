279bbd727e856e081e0fca6d3dade18ff86a443e
Vasiliy-Makogon <info@mirgorod.ru>
2017-12-08 18:39:39 +0300

    Update Exception.php

--- Mysql/Exception.php
+++ Mysql/Exception.php
@@ -3,6 +3,8 @@
  * @author Vasiliy Makogon, makogon-vs@yandex.ru
  * @link https://github.com/Vasiliy-Makogon/Database/
  */
+namespace Krugozor\Database\Mysql;
+
 class Database_Mysql_Exception extends Exception
 {
 
